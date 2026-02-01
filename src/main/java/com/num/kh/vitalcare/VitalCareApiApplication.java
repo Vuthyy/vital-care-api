4cd9ac2836e2862e23eccaca65c8cce3e2acfb3d
Vuthyy <141915285+Vuthyy@users.noreply.github.com>
2026-02-01 18:41:34 +0700

    Initial commit

---
+++ src/main/java/com/num/kh/vitalcare/VitalCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.num.kh.vitalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/num/kh/vitalcare/VitalCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.num.kh.vitalcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
